7c5ae4ce997c0c0d3f7b7be2c8540267c66aa33f
Naomi <naomi@admins-air.home>
2015-07-27 21:30:26 +0100

    lesson1

--- client/templates/naomi/code/lesson1.js
+++ client/templates/naomi/code/lesson1.js
@@ -1,15 +1,67 @@
 // lesson1.js
 
-Template.lesson1.helpers({});
+var lesson1 = {
+
+	calculate: function(){
+		this.updateSum();
+		this.updateMulti();	
+		this.updateDivi();
+	},
+
+
+	updateSum: function(){
+		var a = Session.get('a');
+		var b = Session.get('b');
+		var total = parseInt(a) + parseInt(b);
+		Session.set('sumTotal', total);
+	},
+
+	updateMulti: function(){
+		var a = Session.get('a');
+		var b = Session.get('b');
+		var total = parseInt(a) * parseInt(b);
+		Session.set('multiTotal', total);
+	},
+
+	updateDivi: function(){
+		var a = Session.get('a');
+		var b = Session.get('b');
+		var total = parseInt(a) / parseInt(b);
+		Session.set('diviTotal', total);
+	}
+
+};
+
+
+Template.lesson1.helpers({
+
+	sumTotal: function(){
+		return Session.get('sumTotal');
+	},
+
+  multiTotal: function(){
+		return Session.get('multiTotal');
+	},
+
+	diviTotal: function(){
+		return Session.get('diviTotal');
+	}
+
+
+
+});
 
 Template.lesson1.events({
 
-	"submit #mathform": function (event) {
-    event.preventDefault();
-    var a = event.target.a.value;
-    var b = event.target.b.value;
-    event.target.c.value = parseInt(a)+parseInt(b);
-  }
+	"change #a": function (event) {	
+		Session.set('a',event.target.value);
+		lesson1.calculate();
+	},
+
+	"change #b": function (event) {	
+		Session.set('b',event.target.value);
+		lesson1.calculate();
+	}
 
 });
 
